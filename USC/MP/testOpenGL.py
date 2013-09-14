5a10f13545df4db408a39389bca1a2abb56faf88
Mavin Martin <Mavin.Martin@us.usana.com>
2013-09-14 15:06:50 -0600

    Updated new layout for easier functionality

--- USC/MP/testOpenGL.py
+++ USC/MP/testOpenGL.py
@@ -6,21 +6,48 @@ from PyQt4.QtOpenGL import QGLWidget
 import OpenGL.GL as gl
 import OpenGL.arrays.vbo as glvbo
 
-class NeuronData():
-    def __init__(self):
-        pass
+
+class CellTypeDataSet():
+    """
+    Set's The data type set into this object
+    dataSet must be a tuple (dataSet,)
+    """
+
+    def __init__(self, dataSet, rgb=(1, 1, 1), xyz=(0, 0, 0)):
+        # create a Vertex Buffer Object with the specified data
+        self.vbo = glvbo.VBO(dataSet)
+        self.count = dataSet.shape[0]
+        self.rgb = rgb
+        self.xyz = xyz
+
+    def setTranslation(self, x, y, z):
+        self.xyz = (x, y, z)
+
+    def setColor(self, r, g, b):
+        self.rgb = (r, g, b)
+
+    def getTranslation(self):
+        return gl.glTranslate(self.xyz[0], self.xyz[1], self.xyz[2])
+
+    def getColor(self):
+        return gl.glColor(self.rgb[0], self.rgb[1], self.rgb[2])
+
+    def getVBO(self):
+        return self.vbo
+
 
 class GLPlotEAWidget(QGLWidget):
     # default window size
     width, height = 600, 600
 
-    def set_data(self, MEA_data, LEA_data):
-        """Load 2D data as a Nx2 Numpy array.
-        """
-        self.MEA_data = MEA_data
-        self.MEA_count = MEA_data.shape[0]
-        self.LEA_data = LEA_data
-        self.LEA_count = LEA_data.shape[0]
+    def __init__(self, dataSets, view=(0,1,0,1)):
+        super(GLPlotEAWidget, self).__init__()
+
+        self.dataSets = dataSets
+        self.tStart = view[0]
+        self.tStop = view[1]
+        self.rangeBot = view[2]
+        self.rangeTop = view[3]
 
     def set_view_size(self, tStart, tStop, rangeBot, rangeTop):
         """Set's the size of the view space
@@ -33,38 +60,30 @@ class GLPlotEAWidget(QGLWidget):
 
     def initializeGL(self):
         """Initialize OpenGL, VBOs, upload data on the GPU, etc.
-                """
+        """
         # background color
         gl.glClearColor(0, 0, 0, 0)
-        # create a Vertex Buffer Object with the specified data
-        self.mea_vbo = glvbo.VBO(self.MEA_data)
-        self.lea_vbo = glvbo.VBO(self.LEA_data)
 
     def paintGL(self):
         """Paint the scene.
         """
         # clear the buffer
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        # set blue color for subsequent drawing rendering calls
-        gl.glColor(0, 0, 1)
-        # bind the VBO
-        self.mea_vbo.bind()
-        # tell OpenGL that the VBO contains an array of vertices
-        gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
-        # these vertices contain 2 single precision coordinates
-        gl.glVertexPointer(2, gl.GL_FLOAT, 0, self.mea_vbo)
-        # draw "count" points from the VBO
-        gl.glDrawArrays(gl.GL_POINTS, 0, self.MEA_count)
 
-        gl.glColor(1, 0, 0)
-        # bind the VBO
-        self.lea_vbo.bind()
-        # tell OpenGL that the VBO contains an array of vertices
-        gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
-        # these vertices contain 2 single precision coordinates
-        gl.glVertexPointer(2, gl.GL_FLOAT, 0, self.lea_vbo)
-        # draw "count" points from the VBO
-        gl.glDrawArrays(gl.GL_POINTS, 0, self.LEA_count)
+        for dSet in self.dataSets:
+            # set blue color for subsequent drawing rendering calls
+            dSet.getColor()
+            gl.glPushMatrix()
+            dSet.getTranslation()
+            # bind the VBO
+            dSet.getVBO().bind()
+            # tell OpenGL that the VBO contains an array of vertices
+            gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+            # these vertices contain 2 single precision coordinates
+            gl.glVertexPointer(2, gl.GL_FLOAT, 0, dSet.getVBO())
+            # draw "count" points from the VBO
+            gl.glDrawArrays(gl.GL_POINTS, 0, dSet.count)
+            gl.glPopMatrix()
 
     def resizeGL(self, width, height):
         """Called upon window resizing: reinitialize the viewport.
@@ -78,6 +97,11 @@ class GLPlotEAWidget(QGLWidget):
         gl.glLoadIdentity()
 
         gl.glOrtho(self.tStart, self.tStop, self.rangeBot, self.rangeTop, -1, 1)
+
+    def updateGL(self):
+        for dSet in self.dataSets:
+            dSet.getVBO()[0].x += 1
+            pass
 
 ### TODO - THIS IS AN EXAMPLE OF MULTIPLE WIDGETS IN ONE WINDOW
 # class GLPlotEAWidget(QGLWidget):
@@ -146,9 +170,10 @@ if __name__ == '__main__':
             # generate random data points
             self.data = data
             # initialize the GL widget
-            self.widget = GLPlotEAWidget()
-            self.widget.set_data(self.data, self.data)
-            self.widget.set_view_size(0, 10, 0, 10)
+            set1 = CellTypeDataSet(self.data, rgb=(0, 0, 1))
+            set2 = CellTypeDataSet(self.data, rgb=(1, 0, 0), xyz=(5, 0, 0))
+            dataSets = (set1, set2)
+            self.widget = GLPlotEAWidget(dataSets, (0, 10, 0, 10))
             # put the window at the screen position (100, 100)
             self.setGeometry(100, 100, self.widget.width, self.widget.height)
             self.setCentralWidget(self.widget)
